71b84612e096981581068f0298006bfbbd0bf2c9
AdrianGogyan <c7sxt8@gmail.com>
2024-11-05 12:08:17 +0100

    first commit from vsc

---
+++ src/App.jsx
@@ -0,0 +1,66 @@
+import React, { useState } from 'react';
+import './App.css';
+import './Mobile.css';
+import ApiFetching from './components/apiFetching';
+import { FaSearch, FaAlignJustify } from 'react-icons/fa';
+
+function App() {
+
+  // const [searchParam, setSearchParam] = useState('');
+  const [searchWord, setSearchWord] = useState('');
+  const [showRes, setShowRes] = useState(false);
+  const [searchParam, setSearchParam] = useState('');
+
+  function handleKeyDown(e){
+    if(e.key === 'Enter'){
+      
+      searchFunc();
+      console.log('keyPressed');
+    }
+  }
+
+  function searchFunc(){
+
+    console.log('searching for:', searchWord);
+    setSearchParam(searchWord);
+    setShowRes(true);
+    setSearchWord('');
+
+  }
+
+
+
+  return (
+    <div>
+      <div className='navbar'>
+        <div className='logoText'>
+          Pixabay
+        </div>
+        <div className='searchInput' >
+          <input 
+            type="search" 
+            value={searchWord}
+            onKeyDown={handleKeyDown}
+            onChange={(e) => setSearchWord(e.target.value)} 
+          />
+          <FaSearch onClick={searchFunc} />
+        </div>
+        <div className='navbarEl'>
+          <button>Images</button>
+          <button>Video</button>
+          <button>Pixabay</button>
+          <button>About</button>
+        </div>
+        <div className='navbarElSmall'>
+          <FaAlignJustify />
+        </div>
+        
+      </div>
+      <div id='searchRes'>
+        {showRes && <ApiFetching searchParam={searchParam} />}
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/apiFetching.jsx
@@ -0,0 +1,53 @@
+import React, { useEffect, useState } from 'react';
+
+const ApiFetching = ({ searchParam }) => {
+    const [images, setImages] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {    
+    const api_key = '33167218-766cc9009bbf26dae43e769e3';
+    const base_api = 'https://pixabay.com/api/';
+    const url = `${base_api}?key=${api_key}&q=${searchParam}&image_type=photo`;
+
+    const fetchData = async ()=> {
+        try {
+            const res = await fetch(url);
+            const data = await res.json();
+            setImages(data.hits);
+        } catch(error){
+            console.error("error fetching data:", error);
+        } finally {
+            setLoading(false);
+        }
+    };
+    if(searchParam){
+        setLoading(true);
+        fetchData();
+    }
+
+    
+
+
+    }, [searchParam]);
+
+    if(loading) return <div>Loading...</div>;
+
+    return(
+        <div className='imageWrapper' >
+            {images.length > 0 ? (
+                images.map((image) => (
+                    <div  key={image.id}>
+                        <img className='imageFromApi' src={image.largeImageURL} alt={image.tags} />
+                    </div>
+                ))
+            ) : (
+                <div> No images found</div>
+            )
+        
+        }
+        </div>
+    );
+
+};
+
+export default ApiFetching;
